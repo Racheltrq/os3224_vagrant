e8d547227f3b6c6c68acd0778355f84df4e2baba
Racheltrq <rt1726@nyu.edu>
2019-02-13 13:58:05 -0500

    part 3

--- xv6/uniq.c
+++ xv6/uniq.c
@@ -102,6 +102,161 @@ void ccat(int fd)
 	}
 }
 
+void dcat(int fd)
+{
+	int n;
+	int curLineOffset = 0;
+
+	int isNew = 1;
+	int firstLine = 1;
+	int count = 0;
+
+	while ((n = read(fd, buf, 1)) > 0) {
+		if ((strcmp(buf, "\n")) == 0) {
+			cLine[curLineOffset] = '\n';
+			if (isNew) {
+				if ((firstLine == 0) && (count > 1)) {
+					printf(1, "%d %s", count, lastNew);
+				}
+				strcpy(lastNew, cLine);
+				strcpy(cLine, "");
+				count = 0;
+			}
+			count += 1;
+			curLineOffset = 0;
+			isNew = 0;
+			firstLine = 0;
+			n = read(fd, buf, 1);
+		}
+
+		cLine[curLineOffset] = buf[0];
+		if (cLine[curLineOffset] != lastNew[curLineOffset]) {
+			isNew = 1;
+		}
+		else if (firstLine == 1) {
+			isNew = 1;
+		}
+		curLineOffset += 1;
+	}
+
+
+	if (n < 0) {
+		printf(1, "unicat: read error \n");
+		exit();
+	}
+
+//	cLine[curLineOffset] = '\n';
+	if (count > 1) {
+		printf(1, "%d %s", count, lastNew);
+	}
+}
+
+void icat(int fd)
+{
+	int n;
+	int curLineOffset = 0;
+
+	int isNew = 1;
+	int firstLine = 1;
+	int count = 0;
+
+	while ((n = read(fd, buf, 1)) > 0) {
+		if ((strcmp(buf, "\n")) == 0) {
+			cLine[curLineOffset] = '\n';
+			if (isNew) {
+				if (firstLine == 0) {
+					printf(1, "%s", lastNew);
+				}
+				strcpy(lastNew, cLine);
+				strcpy(cLine, "");
+				count = 0;
+			}
+			count += 1;
+			curLineOffset = 0;
+			isNew = 0;
+			firstLine = 0;
+			n = read(fd, buf, 1);
+		}
+
+		cLine[curLineOffset] = buf[0];
+		int ascii = (int)(cLine[curLineOffset]);
+		if (ascii > 96){
+			cLine[curLineOffset] = (char)(ascii - 32);
+		}
+		if (cLine[curLineOffset] != lastNew[curLineOffset]) {
+			isNew = 1;
+		}
+		else if (firstLine == 1) {
+			isNew = 1;
+		}
+		curLineOffset += 1;
+	}
+
+
+	if (n < 0) {
+		printf(1, "unicat: read error \n");
+		exit();
+	}
+
+//	cLine[curLineOffset] = '\n';
+	if (isNew) {
+		printf(1, "%s", lastNew);
+	}
+}
+
+void cicat(int fd)
+{
+	int n;
+	int curLineOffset = 0;
+
+	int isNew = 1;
+	int firstLine = 1;
+	int count = 0;
+
+	while ((n = read(fd, buf, 1)) > 0) {
+		if ((strcmp(buf, "\n")) == 0) {
+			cLine[curLineOffset] = '\n';
+			if (isNew) {
+				if (firstLine == 0) {
+					printf(1, "%d %s", count, lastNew);
+				}
+				strcpy(lastNew, cLine);
+				strcpy(cLine, "");
+				count = 0;
+			}
+			count += 1;
+			curLineOffset = 0;
+			isNew = 0;
+			firstLine = 0;
+			n = read(fd, buf, 1);
+		}
+
+		cLine[curLineOffset] = buf[0];
+		int ascii = (int)(cLine[curLineOffset]);
+		if (ascii > 96){
+			cLine[curLineOffset] = (char)(ascii - 32);
+		}
+		if (cLine[curLineOffset] != lastNew[curLineOffset]) {
+			isNew = 1;
+		}
+		else if (firstLine == 1) {
+			isNew = 1;
+		}
+		curLineOffset += 1;
+	}
+
+
+	if (n < 0) {
+		printf(1, "unicat: read error \n");
+		exit();
+	}
+
+//	cLine[curLineOffset] = '\n';
+	if (isNew) {
+		printf(1, "%d %s", count, lastNew);
+	}
+}
+
 int main(int argc, char * argv[]) {
 
 	int fd, i;
@@ -130,7 +285,34 @@ int main(int argc, char * argv[]) {
 			ccat(fd);
 			close(fd);
 		}
+		else if(strcmp(argv[1], "-d") == 0){
+			if ((fd = open(argv[2], 0)) < 0) {
+				printf(1, "cat: cannot open %s\n", argv[2]);
+				exit();
+			}
+			dcat(fd);
+			close(fd);
+		}
+		else if(strcmp(argv[1], "-i") == 0){
+			if ((fd = open(argv[2], 0)) < 0) {
+				printf(1, "cat: cannot open %s\n", argv[2]);
+				exit();
+			}
+			icat(fd);
+			close(fd);
+		}
 	}
+	else if(argc == 4){
+		if ((strcmp(argv[1], "-c") == 0) && (strcmp(argv[2], "-i") == 0)){
+			if ((fd = open(argv[3], 0)) < 0) {
+				printf(1, "cat: cannot open %s\n", argv[2]);
+				exit();
+			}
+			cicat(fd);
+			close(fd);
+		}
+	}
+
 
 	exit();
 //Part2 ends here
